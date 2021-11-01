9bfdcd144f3c62be78c6e9962ab6eb898af27dd1
Anton Tayanovskyy <anton@pulumi.com>
2021-11-01 12:23:12 -0400

    Preserve/rename Applications output for Mac OS if present

--- builder.sh
+++ builder.sh
@@ -3,5 +3,9 @@ mkdir -p $out/bin
 e="$out/bin/t0yv0-emacs"
 echo "#!/usr/bin/env sh" >> $e
 echo "export PATH=$emacs/bin:\$PATH" >> $e
+if [ -d "$emacs/Applications" ]; then
+    mkdir "$out/Applications"
+    cp -r "$emacs/Applications/Emacs.app" "$out/Applications/T0yv0Emacs.app"
+fi
 echo "exec emacsclient -c --alternate-editor=" >> $e
 chmod a+x $e
