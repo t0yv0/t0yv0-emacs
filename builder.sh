39a730ca3ac2b2bf158ca5c767bc42cb0e7357d0
Anton Tayanovskyy <anton@pulumi.com>
2022-05-13 16:03:22 -0400

    Fix PATH in Mac OS version

--- builder.sh
+++ builder.sh
@@ -6,6 +6,22 @@ echo "export PATH=$emacs/bin:\$PATH" >> $e
 if [ -d "$emacs/Applications" ]; then
     mkdir "$out/Applications"
     cp -r "$emacs/Applications/Emacs.app" "$out/Applications/T0yv0Emacs.app"
+
+    # BEGIN rewrite MacOS entrypoint to add Nix PATH entries
+    entry="$out/Applications/T0yv0Emacs.app/Contents/MacOS/Emacs"
+    fixed="./Emacs-fixed"
+
+    head -n 1 "$entry" >> "$fixed"
+    echo "" >> "$fixed"
+    echo "export PATH=\$HOME/bin:\$HOME/.nix-profile/bin:\$PATH" >> "$fixed"
+    echo "" >> "$fixed"
+    tail -n+2 "$entry" >> "$fixed"
+
+    chmod a+w "$entry"
+    cat "$fixed" > "$entry"
+    rm "$fixed"
+    chmod a-w "$entry"
+    # END
 fi
 echo "exec emacsclient -c --alternate-editor=" >> $e
 chmod a+x $e
