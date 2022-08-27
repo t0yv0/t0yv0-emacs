48a96c1323341584e31eceab6caab099c2db187c
Anton Tayanovskyy <anton.tayanovskyy@gmail.com>
2022-08-26 22:34:13 -0400

    Add flakes

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  inputs = {
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-22.05;
+  };
+
+  outputs =
+    { self,
+      nixpkgs
+    }:
+
+    let package = sys:
+          let
+            pkgs = import nixpkgs { system = sys; };
+          in
+            import ./default.nix { pkgs = pkgs; };
+
+    in {
+      packages.x86_64-linux.default = package "x86_64-linux";
+      packages.x86_64-darwin.default = package "x86_64-darwin";
+    };
+}
