fa194cb3363c53520ce0ee0a1deace5d9d9141b5
Anton Tayanovskyy <anton.tayanovskyy@gmail.com>
2022-09-02 21:10:01 -0400

    Fix org-mode

--- default.el
+++ default.el
@@ -159,6 +159,13 @@
     (setq org-agenda-files '("~/my/notes.org"
                              "~/my/gtd.org"
                              "~/my/tickler.org"))
+
+    ;; Make windmove work in Org mode:
+    (add-hook 'org-shiftup-final-hook 'windmove-up)
+    (add-hook 'org-shiftleft-final-hook 'windmove-left)
+    (add-hook 'org-shiftdown-final-hook 'windmove-down)
+    (add-hook 'org-shiftright-final-hook 'windmove-right)
+
     ;; https://orgmode.org/manual/Capture-templates.html#Capture-templates
     (setq org-capture-templates
           '(("t" "Todo [inbox]" entry
