b0722cd6d2a125c86b41df9e0fb2faa936a2a1fb
Anton Tayanovskyy <anton@pulumi.com>
2022-11-15 10:00:50 -0500

    Make fill-column 120 stick

--- default.el
+++ default.el
@@ -30,7 +30,7 @@
 	  (horizontal-scroll-bars . nil))))
 
 (setq column-number-mode t)
-(setq fill-column 120)
+(setq-default fill-column 120)
 (setq-default indent-tabs-mode nil)
 (setq inhibit-splash-screen t)
 (setq inhibit-startup-message t)
