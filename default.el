e21c3481c5c03bda2be474cde74944e4e5342879
Anton Tayanovskyy <anton.tayanovskyy@gmail.com>
2023-04-17 21:07:12 -0400

    Replace goto-line with consult-goto-line

--- default.el
+++ default.el
@@ -76,6 +76,7 @@
 
 ;;; key bindings
 
+(global-set-key (kbd "M-g g") 'consult-goto-line)
 (global-set-key (kbd "C-c d") 't0yv0/diary)
 (global-set-key (kbd "C-c z") 't0yv0/vterm-repeat)
 (global-set-key (kbd "C-c o") 'org-capture)
