3ea9b1775ddb375fdb706f7496b39864ea15a16f
Anton Tayanovskyy <anton.tayanovskyy@gmail.com>
2023-05-25 22:25:37 -0400

    More convenient recent buffer nav

--- default.el
+++ default.el
@@ -81,6 +81,8 @@
 (global-set-key (kbd "C-c z") 't0yv0/vterm-repeat)
 (global-set-key (kbd "C-c o") 'org-capture)
 (global-set-key (kbd "C-c ?") 't0yv0/cheatsheet)
+(global-set-key (kbd "C-x C-<left>") 't0yv0/window-buffer-back)
+(global-set-key (kbd "C-x C-<right>") 't0yv0/window-buffer-forward)
 
 (global-set-key (kbd "RET") 'newline-and-indent)
 
