bd44bcfafc14103cf644e57d57fa3b6f62f36ebc
Anton Tayanovskyy <anton.tayanovskyy@gmail.com>
2023-08-01 22:08:53 -0400

    Expose intermediate build products for build debugging

--- flake.nix
+++ flake.nix
@@ -3,20 +3,32 @@
     nixpkgs.url = github:NixOS/nixpkgs/nixos-23.05;
   };
 
-  outputs =
-    { self,
-      nixpkgs
-    }:
+  outputs = { self, nixpkgs }:
+    let
 
-    let package = sys:
-          let
-            pkgs = import nixpkgs { system = sys; };
-          in
-            import ./default.nix { pkgs = pkgs; };
+      systems = [
+        "x86_64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      packages = sys:
+        let
+          pkgs = import nixpkgs { system = sys; };
+        in {
+          default = import ./default.nix { pkgs = pkgs; };
+          default-el = import ./default-el.nix { pkgs = pkgs; };
+          t0yv0-ware = import ./t0yv0-ware.nix { pkgs = pkgs; };
+          copilot = import ./copilot.nix { pkgs = pkgs; };
+          ts = import ./ts.nix { pkgs = pkgs; };
+        };
+
+      pkgsMap = builtins.listToAttrs (builtins.map (sys: {
+        name = sys;
+        value = packages sys;
+      }) systems);
 
     in {
-      packages.x86_64-linux.default = package "x86_64-linux";
-      packages.x86_64-darwin.default = package "x86_64-darwin";
-      packages.aarch64-darwin.default = package "aarch64-darwin";
+      packages = pkgsMap;
     };
 }
