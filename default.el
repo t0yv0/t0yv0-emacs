1888cb9c303cde6ee781e9be9a65753b9e4df685
Anton Tayanovskyy <anton.tayanovskyy@gmail.com>
2023-08-10 21:35:19 -0400

    Rebind window ring to C-c b and C-c f

--- default.el
+++ default.el
@@ -79,8 +79,8 @@
 (global-set-key (kbd "C-c z") 't0yv0/vterm-repeat)
 (global-set-key (kbd "C-c o") 'org-capture)
 (global-set-key (kbd "C-c ?") 't0yv0/cheatsheet)
-(global-set-key (kbd "C-c ,") 't0yv0/window-buffer-back)
-(global-set-key (kbd "C-c .") 't0yv0/window-buffer-forward)
+(global-set-key (kbd "C-c b") 't0yv0/window-buffer-back)
+(global-set-key (kbd "C-c f") 't0yv0/window-buffer-forward)
 
 (global-set-key (kbd "RET") 'newline-and-indent)
 
