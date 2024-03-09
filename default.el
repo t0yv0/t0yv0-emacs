7715eaa774377dba53e9523090048a5c6150dab0
Anton Tayanovskyy <anton.tayanovskyy@gmail.com>
2024-03-08 19:42:27 -0500

    Use org-roam on startup to make C-c r work

--- default.el
+++ default.el
@@ -436,11 +436,12 @@
               (set-frame-parameter nil 'unsplittable t))))
 
 (use-package org-roam
-  :after (major-mode-hydra org)
+  :bind (("C-c r" . t0yv0/org-roam-hydra/body))
   :custom
   (org-roam-directory (file-truename "~/workshare/org-roam/"))
-  :bind (("C-c r" . t0yv0/org-roam-hydra/body))
   :config
+  (require 'org)
+  (require 'major-mode-hydra)
   ;; If you're using a vertical completion framework, you might want a more informative completion interface
   (setq org-roam-node-display-template (concat "${title:*} " (propertize "${tags:10}" 'face 'org-tag)))
   (org-roam-db-autosync-mode)
