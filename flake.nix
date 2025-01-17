e780449e211491526329cb83dde6033e3d489102
Anton Tayanovskyy <anton.tayanovskyy@gmail.com>
2025-01-16 22:21:12 -0500

    git-commit is no longer needed

--- flake.nix
+++ flake.nix
@@ -109,7 +109,6 @@
           epkgs.embark
           epkgs.embark-consult
           epkgs.expand-region
-          epkgs.git-commit
           epkgs.git-link
           epkgs.go-mode
           epkgs.haskell-mode
